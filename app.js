d0a81698b3c4ab043002e1da31c93621b8d3a856
micahchiu <micahchiu@ingx.net>
2018-05-05 17:55:58 +0800

    update bug

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
-
-const debug = require('debug')('egg-passport-local');
-const LocalStrategy = require('passport-multi-type').Strategy;
+ 
+const LocalStrategy = require('passport-multi-type');
 
 module.exports = app => {
-  const config = app.config.passportLocal;
+  const config = app.config.passportMultiType;
   config.passReqToCallback = true;
-
   app.passport.use(new LocalStrategy(config, (req,type, username, password, done) => {
     // format user
     const user = {
@@ -14,10 +12,7 @@ module.exports = app => {
       type,
       username,
       password,
-    };
-    debug('%s %s get user: %j', req.method, req.url, user);
-
-    // let passport do verify and call verify hook
+    }; 
     app.passport.doVerify(req, user, done);
   }));
 };
